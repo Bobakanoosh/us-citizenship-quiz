67b378f214fa751b9e0f53b2386a2186a4c3a70f
Bobakanoosh <jack.bobakanoosh@gmail.com>
2023-02-25 02:03:41 -0600

    Add "show answer" text

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -6,10 +6,11 @@ const showAnswer = ref(false);
 </script>
 
 <template>
-	<div class="flex flex-col gap-y-4 bg-green-700 p-3 rounded-lg text-lg" @click="showAnswer = !showAnswer">
+	<button class="flex flex-col gap-y-4 bg-green-700 p-3 rounded-lg text-lg" @click="showAnswer = !showAnswer">
 		<span class="font-medium">{{ question.question }}</span>
-		<div v-if="showAnswer" class="flex flex-col gap-y-0.5">
+		<div v-if="showAnswer" class="flex flex-col text-left gap-y-0.5">
 			<span v-for="(answer, index) of question.answers" :key="index">{{ `${index + 1}. ${answer}` }}</span>
 		</div>
-	</div>
+		<span v-if="!showAnswer" class="text-sm">Show Answer</span>
+	</button>
 </template>
