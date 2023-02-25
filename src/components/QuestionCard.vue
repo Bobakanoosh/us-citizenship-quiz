0237c50ef776d56780c0905adb9d3771a1cffd03
Bobakanoosh <jack.bobakanoosh@gmail.com>
2023-02-25 02:08:55 -0600

    fix question center aligned text

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -6,9 +6,9 @@ const showAnswer = ref(false);
 </script>
 
 <template>
-	<button class="flex flex-col gap-y-4 bg-green-700 p-3 rounded-lg text-lg" @click="showAnswer = !showAnswer">
+	<button class="flex flex-col gap-y-4 bg-green-700 p-3 rounded-lg text-lg text-left" @click="showAnswer = !showAnswer">
 		<span class="font-medium">{{ question.question }}</span>
-		<div v-if="showAnswer" class="flex flex-col text-left gap-y-0.5">
+		<div v-if="showAnswer" class="flex flex-col gap-y-0.5">
 			<span v-for="(answer, index) of question.answers" :key="index">{{ `${index + 1}. ${answer}` }}</span>
 		</div>
 		<span v-if="!showAnswer" class="text-sm">Show Answer</span>
